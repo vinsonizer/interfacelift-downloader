02cf9119e75cfb1b265fdf3df90eb2372622ad89
Jason Vinson <vinsonizer@gmail.com>
2016-04-21 09:08:37 -0400

    Updated to use properties file and cleaned up warnings

--- README.md
+++ README.md
@@ -3,10 +3,6 @@ interfacelift-downloader
 
 You may want to change your file path in src/main/resources/downloader.properties
  
-To run on cygwin:
-
-`sbt run && for i in $(cat files.txt); do cygstart $(cygpath -u $i); done`
-
-or with maven:
+To run on cygwin with maven:
 
 `mvn compile exec:java -Dexec.mainClass=com.shemasoft.interfacelift.downloader.InterfaceliftDownloader`
--- src/main/scala/com/shemasoft/interfacelift/downloader/InterfaceliftDownloader.scala
+++ src/main/scala/com/shemasoft/interfacelift/downloader/InterfaceliftDownloader.scala
@@ -1,25 +1,41 @@
 package com.shemasoft.interfacelift.downloader
 
-import java.io.{BufferedOutputStream, File, FileOutputStream, InputStream, FileWriter}
+import java.io.{BufferedOutputStream, File, FileOutputStream, FileWriter, InputStream}
 import java.net.{URL, URLConnection}
 import java.util
-import java.util.Comparator
+import java.util.{Comparator, Properties}
 
-import scala.xml.{Node, NodeSeq, Text }
+import scala.xml.{Node, NodeSeq, Text}
 import org.xml.sax.InputSource
+
+import scala.io.Source
 
 class InterfaceliftDownloader {
 
   val parser = new InterfaceliftHtmlParser
+
+  def readProperties() : Properties = {
+    val props = new Properties()
+    props.load(Source.fromFile("src/main/resources/downloader.properties").reader())
+    props
+  }
+
+  val configProperties = readProperties()
   
-  def getImages(targetFolder: String, baseUrl: String) = {
+  def downLoadImages() = {
+    trimOld()
+    val targetFolder = configProperties.getProperty("targetFolder")
+    val baseUrl = configProperties.getProperty("baseUrl")
+    val numberOfPages = configProperties.getProperty("numPages").toInt
     println("Downloading from " + baseUrl + " to " + targetFolder)
-    val xml = getUrlContent(baseUrl)
-    //val pageNums = getPageNums(xml)
-    val pageNums = 10
-    val imageUrls = Range(1, pageNums+1, 1).map(idx => {getImageUrls(getUrlContent(baseUrl + "index" + idx + ".html"))}).flatten
-    val imagesProcessed = imageUrls.filter(_.endsWith(".jpg")).filter(_.startsWith("/wallpaper")).distinct.map(downloadImage(_,
-      targetFolder)).filter(_.length > 0)
+    val imageUrls = Range(1, numberOfPages+1, 1).flatMap(idx => {getImageUrls(getUrlContent(baseUrl + "index" + idx + ".html"))})
+
+    val imagesProcessed = imageUrls
+      .filter(_.endsWith(".jpg"))
+      .filter(_.startsWith("/wallpaper"))
+      .distinct.map(downloadImage(_, targetFolder))
+      .filter(_.length > 0)
+
     writeCompletionFile(targetFolder, imagesProcessed)
     println(imagesProcessed.size + " of " + imagesProcessed.size + " downloaded")
   }
@@ -27,8 +43,8 @@ class InterfaceliftDownloader {
   def writeCompletionFile(targetFolder: String, files: Seq[String]) = {
     val output = new FileWriter("files.txt", false)
     files.foreach(x => { output.write(targetFolder + File.separatorChar + x + "\n") })
-    output.flush
-    output.close
+    output.flush()
+    output.close()
   }
   
   def downloadImage(imgUrl: String, targetFolder: String) = {
@@ -41,8 +57,8 @@ class InterfaceliftDownloader {
         val buffer:Array[Byte] = new Array[Byte](1024)
 	    Iterator.continually(stream.read(buffer)).takeWhile(_ != -1).foreach(n => fileStream.write(buffer,0,n))
       })
-      fileStream.flush
-      fileStream.close
+      fileStream.flush()
+      fileStream.close()
       imgUrl.substring(imgUrl.lastIndexOf("/"))
     } else {
       ""
@@ -50,12 +66,12 @@ class InterfaceliftDownloader {
   }
   
   def getImageUrls(xml: NodeSeq) = {
-    (findAttrBy((xml \\ "div"), "class", _.equals("download")) \\ "a").map(_.attribute("href").getOrElse(Text(""))).map(_.text)
+    (findAttrBy(xml \\ "div", "class", _.equals("download")) \\ "a").map(_.attribute("href").getOrElse(Text(""))).map(_.text)
   }
   
   def getPageNums(xml: Node) = {
-    val pageLinks = (findAttrBy((xml \\ "div"), "class", _.equals("pagenums_bottom")) \\ "a").toArray
-    pageLinks(pageLinks.size - 2).text.toInt
+    val pageLinks = (findAttrBy(xml \\ "div", "class", _.equals("pagenums_bottom")) \\ "a").toArray
+    pageLinks(pageLinks.length - 2).text.toInt
   }
   
   def findAttrBy(xml: NodeSeq, label: String, check: String => Boolean) = {
@@ -75,15 +91,17 @@ class InterfaceliftDownloader {
     connection.setRequestProperty("user-agent", "Mozilla")
     val inputStream = connection.getInputStream
     val result = func(inputStream)
-    inputStream.close
+    inputStream.close()
     result
   }
 
-  def trimOld(directory: String, amount: Int) = {
+  def trimOld() = {
+    val directory = configProperties.getProperty("targetFolder")
+    val amount = configProperties.getProperty("trimFactor").toInt
     val imgDirFiles = new File(directory).listFiles()
     util.Arrays.sort(imgDirFiles, new Comparator[File]() {
       override def compare(o1: File, o2: File): Int = {
-        return o1.lastModified().compareTo(o2.lastModified());
+        o1.lastModified().compareTo(o2.lastModified())
       }
     })
     imgDirFiles.take(amount).foreach(_.delete())
@@ -95,10 +113,7 @@ object InterfaceliftDownloader {
 
   def main(args: Array[String]) = {
     val d = new InterfaceliftDownloader
-    val target ="""C:\Users\jv\Pictures\InterfaceLift"""
-    d.trimOld(target, 20)
-    //d.getImages("/home/jason/Pictures/InterfaceLift","http://interfacelift.com/wallpaper/downloads/rating/widescreen/1440x900/")
-    d.getImages(target,"http://interfacelift.com/wallpaper/downloads/rating/widescreen/1600x900/")
+    d.downLoadImages()
   }
 
 }
